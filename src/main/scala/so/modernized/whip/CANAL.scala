9b7c93cff0a9b4167b92eb02cde371850fd51bb8
John Sullivan <john.t.sullivan@gmail.com>
2015-07-23 12:48:37 -0400

    Basic memoized implementation of deltaMeasure and Simlink.

--- src/main/scala/so/modernized/whip/CANAL.scala
+++ src/main/scala/so/modernized/whip/CANAL.scala
@@ -3,80 +3,101 @@ package so.modernized.whip
 import scala.collection.mutable
 import scala.reflect.ClassTag
 
-class RangeSeqOrdering[A,C <% Ordered[C]](getOrdered:A => C) extends Ordering[Seq[A]] {
-  private def getBounds(s:Set[A]) = {
-    val sq = s.toSeq.map(getOrdered).sorted
-    sq.head -> sq.last
+
+class IndexedVertexSeq(val value:Seq[Vertex], val idx:Int) extends (Seq[Vertex], Int) {
+  override def hashCode = idx
+  override def equals(obj:Any) = obj match {
+    case ref:AnyRef if this eq ref => true
+    case that:IndexedVertexSeq => this.idx == that.idx
+    case otw => false
   }
-  override def compare(x: Seq[A], y: Seq[A]) = {
-    val xStart = getOrdered(x.head)
-    val xEnd = getOrdered(x.last)
-    val yStart = getOrdered(y.head)
-    val yEnd = getOrdered(y.last)
-    if(xEnd < yStart) {
-      -1
-    } else if (yEnd < xStart) {
-      1
-    } else if (xStart.compare(yStart) == 0 && xEnd.compare(yEnd) == 0) {
-      0
-    } else {
-      throw new IllegalArgumentException("Orderable Range Seqs must not overlap")
-    }
-  }
+  lazy val valueSet = value.toSet
+
+  def ++(other:IndexedVertexSeq, newIdx:Int) = new IndexedVertexSeq(this.value ++ other.value, newIdx)
 }
 
-/**
- * Takes a distance measure on pairs and orders them by the size of that measure
- */
-class PairDistanceOrdering[A, C <% Ordered[C]](dist:((A, A) => C)) extends Ordering[(A, A)] {
-  override def compare(x: (A, A), y: (A, A)): Int = dist.tupled(x) compare dist.tupled(y)
+trait StandardMemoizedSimlink {
+  this: CANAL =>
+
+  private val participationMemo = mutable.HashMap.empty[(IndexedVertexSeq, IndexedVertexSeq), Int]
+
+  private def participation(xs:IndexedVertexSeq, ys:IndexedVertexSeq) = {
+    participationMemo.getOrElseUpdate(xs -> ys,xs.valueSet.count { x =>
+      x.edges.exists{ e =>
+        ys.valueSet.contains(e.to)
+      }
+    })
+  }
+
+  private def participationRatio(xs:IndexedVertexSeq, ys:IndexedVertexSeq) = (participation(xs, ys) + participation(ys, xs)).toDouble / (xs.value.size + ys.value.size)
+
+  // this call participation many times, but participation's result is memoized
+  def smallDelta(xs:IndexedVertexSeq, ys:IndexedVertexSeq) = if(participationRatio(xs, ys) <= 0.5) {
+    participation(xs, ys)
+  } else {
+    ys.value.size - participation(xs, ys)
+  }
+
+  def deltaMeasure(grouping:Seq[IndexedVertexSeq]) = grouping.flatMap { xs =>
+    grouping.map { ys =>
+      smallDelta(xs, ys)
+    }
+  }.sum
+
+  def simLink(i:IndexedVertexSeq, j:IndexedVertexSeq, grouping:Seq[IndexedVertexSeq]) = {
+    grouping.collect { case k if k != i && k != j =>
+      math.abs(participationRatio(i,k) - participationRatio(j, k))
+    }.sum
+  }
 }
 
 trait CANAL {
 
   // when implemented, this should be memoized for speed
-  def simLink(v1s:Iterable[Vertex], v2s:Iterable[Vertex]):Double
-  def deltaMeasure(grouping:Iterable[Seq[Vertex]]):Double
-  def mu(d:Double, dm1:Double) = (d - dm1)/dm1
+  def simLink(v1s:IndexedVertexSeq, v2s:IndexedVertexSeq, grouping:Seq[IndexedVertexSeq]):Double
+  def deltaMeasure(grouping:Seq[IndexedVertexSeq]):Int
+  def mu(d:Int, dm1:Int) = (d - dm1).toDouble/dm1
 
-  def run2[C <% Ordered[C] : ClassTag](g:Graph, numCats:Int, getOrdered:(Vertex => C)): Seq[C] = {
-    def updateSimlinkPairs(grouping:Seq[(Seq[Vertex], Int)]) = grouping.sliding(2).collect{ case Seq(pair1, pair2) => pair1 -> pair2 }.toSeq.sortBy { case ((v1, _), (v2, _)) => simLink(v1, v2) }
+  def run[C <: Ordered[C] : ClassTag](g:Graph, numCats:Int, getOrdered:(Vertex => C))(implicit toOrdered:C => Ordered[C]) = {
+    def updateSimlinkPairs(grouping:Seq[IndexedVertexSeq]) = grouping.sliding(2).collect{ case Seq(pair1, pair2) => pair1 -> pair2 }.toSeq.sortBy { case (v1, v2) => simLink(v1, v2, grouping) }
 
-    var currentGrouping = g.vertices.groupBy(getOrdered).values.toSeq.map(_.toSeq).sorted(new RangeSeqOrdering(getOrdered)).zipWithIndex
+    //var currentGrouping = g.vertices.groupBy(getOrdered).values.toSeq.map(_.toSeq).sorted(new RangeSeqOrdering(getOrdered)).zipWithIndex
+    var currentGrouping = g.vertices.groupBy(getOrdered).toSeq.sortBy(_._1).zipWithIndex.map { case ((_, vs),idx) => new IndexedVertexSeq(vs.toSeq, idx) }
+
     var remainingPairs = updateSimlinkPairs(currentGrouping)
 
-    var deltaPminusOne = null.asInstanceOf[Double]
-    var deltaP = deltaMeasure(currentGrouping.map(_._1))
+    var deltaPminusOne = null.asInstanceOf[Int]
+    var deltaP = deltaMeasure(currentGrouping)
 
     val numLeaves = currentGrouping.length
     var currentIdx = numLeaves
     val merges = new Array[(Int, Int, Double)](numLeaves -1)
     val mergeHeap = new mutable.PriorityQueue[(Int, Int, Double)]()(Ordering.by({tup:(Int, Int, Double) => tup._3}))
-    val subclusters = new Array[Seq[Vertex]]((numLeaves * 2) - 1)
+    val subclusters = new Array[IndexedVertexSeq]((numLeaves * 2) - 1)
     currentGrouping foreach {case (v, i) => subclusters(i) = v}
 
     while(remainingPairs.nonEmpty) {
-      val ((m1, idx1), (m2, idx2)) = remainingPairs.head
+      val (m1, m2) = remainingPairs.head
       currentGrouping = currentGrouping.sliding(2).collect {
         case Vector(v1, v2) if v1 != m1 && v1 != m2 => v1
         case Vector(v1, v2) if v1 == m1 =>
           val parentIdx = currentIdx
           currentIdx += 1
-          val newCluster = m1 ++ m2
+          val newCluster = m1 ++ (m2, parentIdx)
           subclusters(parentIdx) = newCluster
-          newCluster -> parentIdx
+          newCluster
       }.toSeq
       remainingPairs = updateSimlinkPairs(currentGrouping)
 
       deltaPminusOne = deltaP
-      deltaP = deltaMeasure(currentGrouping.map(_._1))
+      deltaP = deltaMeasure(currentGrouping)
 
-      mergeHeap.enqueue((idx1, idx2, mu(deltaP, deltaPminusOne)))
+      mergeHeap.enqueue((m1.idx, m2.idx, mu(deltaP, deltaPminusOne)))
     }
 
     val cutoffs = (0 until numCats).map { _ =>
       val (v1, _, _) = mergeHeap.dequeue()
-      getOrdered(subclusters(v1).last)
+      getOrdered(subclusters(v1).value.last)
     }
     cutoffs
   }
@@ -93,60 +114,4 @@ trait Graph {
 
   def vertices:Iterable[Vertex]
 
-}
-
-class Dendrogram[A](nodes:Array[Seq[A]]) {
-
-  private val numNodes = (nodes.length * 2) - 1
-  private val merges = new Array[(Int, Int, Double)](nodes.length - 1)
-  private var currentIdx = nodes.length
-
-  def nextId = currentIdx
-
-  def mergeId(n1:Int, n2:Int, distance:Double):Int = {
-    val parentId = currentIdx
-    currentIdx += 1
-    merges(parentId - nodes.length) = (n1, n2, distance)
-    parentId
-  }
-
-  //@inline
-  //def children(parent:Int):Seq[A] = allNodes(parent)._1
-
-  //@inline
-  //def index(a:Seq[A]):Int = nodeIdxMap(a)
-
-  def toDotString(composeString:Set[A] => String = _.mkString(", ")):String = {
-    val sb = new StringBuilder("digraph Dendrogram {\n")
-    val edgeString = "\ti%s -> i%s [label=\"%.2f\"];\n"
-    val leafString = "\ti%s [label=\"%s\"];\n"
-
-    merges.zipWithIndex foreach { case t@((c1, c2, score), idx) =>
-      val parent = idx + nodes.length
-      sb append edgeString.format(c1, parent, score)
-      sb append edgeString.format(c2, parent, score)
-    }
-    sb.append("\n")
-    nodes.zipWithIndex foreach { case (elem, idx) =>
-        sb append leafString.format(idx, composeString(elem))
-    }
-    sb.append("}")
-    sb.toString()
-  }
-}
-
-object Dendrogram {
-  def main(args:Array[String]): Unit = {
-    val dend = new Dendrogram(Array("a", "b", "c", "d", "e", "f"))
-
-    dend.mergeId(0,1,0.1)
-    dend.mergeId(2,3,0.3)
-    dend.mergeId(7,4,0.6)
-    dend.mergeId(6,5,0.7)
-    dend.mergeId(9,8,0.8)
-
-    //println(dend.toDotString)
-    println(dend.groupings(3))
-    //println(dend.children(8))
-  }
 }
